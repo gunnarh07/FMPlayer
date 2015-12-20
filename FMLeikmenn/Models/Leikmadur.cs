7a6ba3938181faa6c6c4518d78eb67bba7a4e37e
Gunnar Helgason <gunnarh07@ru.is>
2015-12-20 20:21:45 +0000

    Added average rating over main positions

--- FMLeikmenn/Models/Leikmadur.cs
+++ FMLeikmenn/Models/Leikmadur.cs
@@ -12,8 +12,8 @@ namespace FMLeikmenn.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        //public string NameOfTeamThatPlayerIsContractedTo { get; set; }
         [DataType(DataType.Date)]
-        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd", ApplyFormatInEditMode = true)]
         public DateTime DateStamp { get; set; }
         public string Position { get; set; }
         public int Corners { get; set; }
@@ -65,7 +65,209 @@ namespace FMLeikmenn.Models
                     Positioning + Teamwork + Vision + WorkRate + Acceleration + Agility + Balance + JumpingReach + 
                     NatuaralFitnes + Pace + Stamina + Strength)/36;
             }
-}
+        }
+
+        public double LiberoSupport
+        {
+            get
+            {
+                return (Dribbling + Heading + Marking + Passing + Tackling + Anticipation + Bravery + Composure + Concentration + 
+                    Decisions + Determination + Positioning + Teamwork + Vision + Acceleration + Agility + Balance + JumpingReach +
+                    Strength) / 19;
+            }
+        }
+
+        public double BallPlayingDefender
+        {
+            get
+            {
+                return (FirstTouch + Heading + Marking + Passing + Tackling + Technique + Anticipation + Bravery + Composure + Concentration + Decisions + 
+                    Determination + Positioning + Vision + Acceleration + JumpingReach + Strength) / 17;
+            }
+        }
+
+        public double CentralDefender
+        {
+            get
+            {
+                return (Heading + Marking + Tackling + Aggression + Anticipation + Bravery + Concentration + Decisions +
+                    Determination + Positioning + JumpingReach + Strength) / 12;
+            }
+        }
+
+        public double LimitedCentralDefender
+        {
+            get
+            {
+                return (Heading + Marking + Tackling + Determination + Positioning + JumpingReach + Strength) / 7;
+            }
+        }
+
+        public double CompleteWingBack
+        {
+            get
+            {
+                return (Crossing + Dribbling + FirstTouch+ + Marking + Passing + Tackling + Technique + 
+                    Anticipation + Composure + Concentration + Decisions + Flair + OffTheBall + Positioning + Teamwork + WorkRate +
+                    Acceleration + Agility + Balance + Pace + Stamina) / 21;
+            }
+        }
+
+        public double FullBackDefend
+        {
+            get
+            {
+                return (Marking + Tackling +
+                    Anticipation  + Concentration + Positioning + Teamwork +
+                    Acceleration + Stamina) / 8;
+            }
+        }
+
+        public double FullBackSuport
+        {
+            get
+            {
+                return (Crossing + Marking + Passing + Tackling +
+                    Anticipation + Concentration + Positioning + Teamwork + WorkRate +
+                    Acceleration + Stamina) / 11;
+            }
+        }
+
+        public double FullBackAttack
+        {
+            get
+            {
+                return (Crossing + Dribbling + FirstTouch + Marking + Tackling +
+                    Anticipation + Concentration + OffTheBall + Positioning + Teamwork + WorkRate +
+                    Acceleration + Pace + Stamina) / 14;
+            }
+        }
+
+        public double LimitedFullBack
+        {
+            get
+            {
+                return (Marking + Tackling +
+                    Anticipation + Concentration + Decisions + Positioning + Teamwork + 
+                    Stamina) / 8;
+            }
+        }
+
+        public double AnchorManDefend
+        {
+            get
+            {
+                return (Heading + Marking + Passing + Tackling +
+                    Anticipation + Composure + Concentration + Decisions + Determination + Positioning +
+                    Strength) / 11;
+            }
+        }
+
+        public double BallWinningMidfielder
+        {
+            get
+            {
+                return (Marking + Tackling +
+                    Aggression + Bravery + Determination + Positioning + Teamwork + WorkRate +
+                    Acceleration + Stamina + Strength) / 12;
+            }
+        }
+
+        public double BoxToBoxMidfielderSupport
+        {
+            get
+            {
+                return (Finishing + FirstTouch + LongShots + Passing + Tackling +
+                    Anticipation + Bravery + Composure + Concentration + Decisions + Determination + OffTheBall + Positioning + WorkRate +
+                    Acceleration + Balance + NatuaralFitnes + Stamina + Strength) / 19;
+            }
+        }
+
+        public double DeepLyingPlaymakerDefender
+        {
+            get
+            {
+                return (FirstTouch + Passing + Technique +
+                    Composure + Decisions + Positioning + Teamwork + Vision +
+                    Balance) / 9;
+            }
+        }
+
+        public double DeepLyingPlaymakerSupport
+        {
+            get
+            {
+                return (FirstTouch + LongShots + Passing + Technique +
+                    Composure + Decisions + OffTheBall + Teamwork + Vision +
+                    Balance) / 10;
+            }
+        }
+
+        public double AdvancedPlaymakerSupport
+        {
+            get
+            {
+                return (FirstTouch + Passing + Technique +
+                    Composure + Decisions + Flair + Teamwork + Vision) / 8;
+            }
+        }
+
+        public double AdvancedPlaymakerAttack
+        {
+            get
+            {
+                return (Dribbling + FirstTouch + Passing + Technique +
+                    Anticipation + Composure + Decisions + Flair + OffTheBall + Teamwork + Vision) / 11;
+            }
+        }
+
+        public double AttackingMidfielderSupport
+        {
+            get
+            {
+                return (FirstTouch + LongShots + Passing + Technique +
+                    Anticipation + Composure + Decisions + OffTheBall +  Positioning + Teamwork + Vision
+                    ) / 11;
+            }
+        }
+
+        public double AttackingMidfielderAttack
+        {
+            get
+            {
+                return (Finishing + FirstTouch + LongShots + Passing + Technique +
+                    Anticipation + Composure + Concentration + Decisions + OffTheBall + Teamwork + Vision
+                    ) / 12;
+            }
+        }
+
+        public double WingerSupport
+        {
+            get
+            {
+                return (Crossing + Dribbling + FirstTouch + Passing + Technique +
+                    WorkRate +
+                    Acceleration + Pace + Stamina) / 9;
+            }
+        }
+
+        public double WingerAttack
+        {
+            get
+            {
+                return (Crossing + Dribbling + FirstTouch + Passing + Technique +
+                    Flair + OffTheBall + WorkRate +
+                    Acceleration + Pace + Stamina) / 11;
+            }
+        }
+
+        public double PoacherAttributes
+        {
+            get
+            {
+                return (Finishing + FirstTouch + Anticipation + Composure + Concentration + OffTheBall + Acceleration + Agility) / 8;
+            }
+        }
         //public IEnumerable<SelectListItem> Positions { get; set; }
     }
     
